a374b94bcc06da63323d47fd091a52a9a4ee4619
Sherin Rafaat <149476292+SherinRafaat@users.noreply.github.com>
2024-03-29 16:04:31 -0700

    Update ICompany.cs
    creating a repo interface

--- ProjectMVC/Repository/ICompany.cs
+++ ProjectMVC/Repository/ICompany.cs
@@ -2,5 +2,16 @@
 {
 	public interface ICompany
 	{
+       public List<Job> GetAll();//string includes=null)
+
+        public Company GetById(int id);
+
+        void Insert(Company  obj);
+
+        void Update(Company  obj);
+
+        void Delete(int id);
+
+		
 	}
 }
